43c9d136184fc3d44ff1101070bc43c23b7b00a0
KristijanTemelkovski97 <kristijan.temelkovski@students.finki.ukim.com>
2020-04-09 17:40:44 +0100

    new class and changed some articles

---
+++ Receipt.java
@@ -0,0 +1,15 @@
+class Receipt{
+    List<Item> itemList;
+
+    public Receipt(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+}
